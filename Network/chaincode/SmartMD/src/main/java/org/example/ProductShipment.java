614f4aacb32348425be98147e135276a2c45074d
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-08-26 17:04:30 +0100

    Update ProductShipment.java

--- Network/chaincode/SmartMD/src/main/java/org/example/ProductShipment.java
+++ Network/chaincode/SmartMD/src/main/java/org/example/ProductShipment.java
@@ -5,6 +5,12 @@ import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.Contract;
 import org.hyperledger.fabric.contract.annotation.Default;
 import org.hyperledger.fabric.contract.annotation.Info;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.util.List;
 
 @Contract(
         name = "Shipment process",
@@ -32,6 +38,45 @@ public class ProductShipment implements ContractInterface {
     read and write state data, and invoke other chaincode functions.
      */
 
-    
+    @Transaction()
+    public Package createPackage(final Context ctx, final String id, final List<Product> products,
+                                 final String from,final String to) {
+        ChaincodeStub stub = ctx.getStub();
 
+        String packageState = stub.getStringState(id);
+        if (!packageState.isEmpty()) {
+            String errorMessage = String.format("Package %s already exists", id);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, PackageErrors.PACKAGE_ALREADY_EXISTS.toString());
+        }
+
+        Package aPackage = new Package(id,products,from,to,from,false);
+        packageState = genson.serialize(aPackage);
+        stub.putStringState(id, packageState);
+
+        return aPackage;
+    }
+
+    @Transaction()
+    public Package changePackageLocation(final Context ctx, final String id, final String current_location) {
+        ChaincodeStub stub = ctx.getStub();
+
+        String packageState = stub.getStringState(id);
+
+        if (packageState.isEmpty()) {
+            String errorMessage = String.format("Package %s does not exist", id);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage, PackageErrors.PACKAGE_ALREADY_EXISTS.toString());
+        }
+
+        Package aPackage = genson.deserialize(packageState, Package.class);
+
+        boolean delivered = aPackage.getTo().matches(current_location);
+
+        Package newPackage = new Package(id,aPackage.getProducts(),aPackage.getFrom(),aPackage.getTo(),current_location,delivered);
+        String newPackageState = genson.serialize(newPackage);
+        stub.putStringState(id, newPackageState);
+
+        return newPackage;
+    }
 }
