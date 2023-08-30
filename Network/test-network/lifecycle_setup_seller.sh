9ddc5a2957f492796826547ed9e06a4eb1507e47
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-08-30 22:27:01 +0100

    update build.gradle

--- Network/test-network/lifecycle_setup_seller.sh
+++ Network/test-network/lifecycle_setup_seller.sh
@@ -2,7 +2,7 @@
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=$PWD/../config/
 export CORE_PEER_TLS_ENABLED=true
-export CORE_PEER_LOCALMSPID="DeliveryMSP"
+export CORE_PEER_LOCALMSPID="SellerMSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/seller.example.com/peers/peer0.seller.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/seller.example.com/users/Admin@seller.example.com/msp
 export CORE_PEER_ADDRESS=localhost:11051
