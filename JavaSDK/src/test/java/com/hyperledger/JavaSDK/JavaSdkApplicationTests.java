cd54b80fef68a24301fe9e725f66635d77908493
BOULOUQAT-JAWAD <boulouqatjawad22@gmail.com>
2023-09-11 23:03:12 +0100

    update JavaSDK

---
+++ JavaSDK/src/test/java/com/hyperledger/JavaSDK/JavaSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperledger.JavaSDK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
